163b2992f4ed03e48efd061e14762b97dc86f67d
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2021-07-09 21:47:10 +0700

    Handle access to endpoint

--- app/views/note.py
+++ app/views/note.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from app import engine
-from flask import Response
+from flask import Response, jsonify
 
 @engine.route("/")
 def root_node():
@@ -15,3 +15,16 @@ def api_node():
         status=403,
         mimetype='text/plain'
     )
+
+@engine.route("/api/<string:endpoint>", methods=["GET","POST"])
+def endpoint_node(endpoint=""):
+    endpoint = endpoint.strip()
+    if not endpoint:
+        return jsonify({
+            "error":"Command is not valid.",
+            "data":None
+        })
+
+    return jsonify({
+        "data":[]
+    })
