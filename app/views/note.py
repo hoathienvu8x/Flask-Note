a41a35a0ca3b719f86abad781b23a3ef6c919ad2
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2021-07-09 21:43:11 +0700

    Denial of service if access to '/api' or '/api/' url

--- app/views/note.py
+++ app/views/note.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from app import engine
+from flask import Response
 
 @engine.route("/")
 def root_node():
     return 'Health check good.'
+
+@engine.route("/api")
+@engine.route("/api/")
+def api_node():
+    return Response(
+        response='Command is not valid',
+        status=403,
+        mimetype='text/plain'
+    )
